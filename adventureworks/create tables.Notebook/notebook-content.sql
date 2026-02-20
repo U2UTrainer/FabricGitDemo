6cdd9c8d98057201b99cf55636621deabfe75dc4
Jurgen Postelmans <jurgen.postelmans@icloud.com>
2026-02-20 11:20:01 +0100

    Committing 1 item from workspace 780e2611-d7b6-4dd2-9d83-12249ce0eec9

---
+++ adventureworks/create tables.Notebook/notebook-content.sql
@@ -0,0 +1,38 @@
+-- Fabric notebook source
+
+-- METADATA ********************
+
+-- META {
+-- META   "kernel_info": {
+-- META     "name": "synapse_pyspark"
+-- META   },
+-- META   "dependencies": {
+-- META     "lakehouse": {
+-- META       "default_lakehouse": "8210848e-d7be-466b-8bb7-36743eb83690",
+-- META       "default_lakehouse_name": "Sales",
+-- META       "default_lakehouse_workspace_id": "780e2611-d7b6-4dd2-9d83-12249ce0eec9",
+-- META       "known_lakehouses": [
+-- META         {
+-- META           "id": "8210848e-d7be-466b-8bb7-36743eb83690"
+-- META         }
+-- META       ]
+-- META     }
+-- META   }
+-- META }
+
+-- CELL ********************
+
+-- Welcome to your new notebook
+-- Type here in the cell editor to add code!
+create TABLE customer
+(
+    id INT
+)
+
+
+-- METADATA ********************
+
+-- META {
+-- META   "language": "sparksql",
+-- META   "language_group": "synapse_pyspark"
+-- META }
